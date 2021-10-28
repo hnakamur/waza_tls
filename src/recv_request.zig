92d18ec69b50d1dc47a2001aba9ff6e3807250ad
Hiroaki Nakamura <hnakamur@gmail.com>
2021-10-28 22:46:11 +0900

    Add buf to RecvRequest

--- src/recv_request.zig
+++ src/recv_request.zig
@@ -7,16 +7,23 @@ const config = @import("config.zig");
 
 /// A receiving request.
 pub const RecvRequest = struct {
+    buf: []const u8,
     method: Method,
     uri: []const u8,
     version: Version,
     headers: []const u8,
     allocator: *mem.Allocator,
 
-    // value of custom `method`, `uri`, and `headers` must be allocated with `allocator`.
-    // they are freed in `deinit`.
-    pub fn init(allocator: *mem.Allocator, method: Method, uri: []const u8, version: Version, headers: []const u8) RecvRequest {
-        return .{
+    /// `buf` must be allocated with `allocator`.
+    /// It will be freed in `deinit`.
+    pub fn init(allocator: *mem.Allocator, buf: []const u8, result: *const RequestLineSplitter.Result, headers_len: usize) !RecvRequest {
+        const method = Method.fromText(buf[0..result.method_len]) catch unreachable;
+        const uri = buf[result.uri_start_pos .. result.uri_start_pos + result.uri_len];
+        const ver_buf = buf[result.version_start_pos .. result.version_start_pos + result.version_len];
+        const version = Version.fromText(ver_buf) catch |_| return error.BadRequest;
+        const headers = buf[result.total_bytes_read .. result.total_bytes_read + headers_len];
+        return RecvRequest{
+            .buf = buf,
             .method = method,
             .uri = uri,
             .version = version,
@@ -26,18 +33,13 @@ pub const RecvRequest = struct {
     }
 
     pub fn deinit(self: *RecvRequest) void {
-        self.allocator.free(self.headers);
-        self.allocator.free(self.uri);
-        switch (self.method) {
-            .custom => |value| self.allocator.free(value),
-            else => {},
-        }
+        self.allocator.free(self.buf);
     }
 };
 
 const version_max_len: usize = Version.http1_1.toText().len;
 
-const RequestLineParser = struct {
+const RequestLineSplitter = struct {
     const State = enum {
         on_method,
         post_method,
@@ -48,86 +50,87 @@ const RequestLineParser = struct {
         done,
     };
 
+    const Result = struct {
+        total_bytes_read: usize = 0,
+        method_len: usize = 0,
+        uri_start_pos: usize = 0,
+        uri_len: usize = 0,
+        version_start_pos: usize = 0,
+        version_len: usize = 0,
+    };
+
     method_max_len: usize = config.method_max_len,
     uri_max_len: usize = config.uri_max_len,
 
     state: State = .on_method,
-    total_bytes_read: usize = 0,
-    method_len: usize = 0,
-    uri_len: usize = 0,
-    version_len: usize = 0,
+    result: Result = undefined,
 
-    pub fn parse(self: *RequestLineParser, chunk: []const u8) !bool {
-        var uri_start_pos: usize = 0;
-        var version_start_pos: usize = 0;
+    pub fn parse(self: *RequestLineSplitter, chunk: []const u8) !bool {
         var pos: usize = 0;
         while (pos < chunk.len) {
             const c = chunk[pos];
             pos += 1;
+            self.result.total_bytes_read += 1;
             switch (self.state) {
                 .on_method => {
                     if (c == ' ') {
-                        self.method_len = self.total_bytes_read + pos - 1;
                         self.state = .post_method;
-                    } else if (!isTokenChar(c) or self.total_bytes_read + pos > self.method_max_len) {
-                        self.total_bytes_read += pos;
-                        return error.BadRequest;
+                    } else {
+                        self.result.method_len += 1;
+                        if (!isTokenChar(c) or self.result.method_len > self.method_max_len) {
+                            return error.BadRequest;
+                        }
                     }
                 },
                 .post_method => {
                     if (c != ' ') {
-                        uri_start_pos = pos - 1;
+                        self.result.uri_start_pos = self.result.total_bytes_read - 1;
+                        self.result.uri_len += 1;
                         self.state = .on_uri;
                     } else {
-                        self.total_bytes_read += pos;
                         return error.BadRequest;
                     }
                 },
                 .on_uri => {
                     if (c == ' ') {
                         self.state = .post_uri;
-                        self.uri_len += pos - 1 - uri_start_pos;
-                    } else if (self.uri_len + pos - uri_start_pos > self.uri_max_len) {
-                        self.total_bytes_read += pos;
-                        return error.UriTooLong;
+                    } else {
+                        self.result.uri_len += 1;
+                        if (self.result.uri_len > self.uri_max_len) {
+                            return error.UriTooLong;
+                        }
                     }
                 },
                 .post_uri => {
                     if (c != ' ') {
-                        version_start_pos = pos - 1;
+                        self.result.version_start_pos = self.result.total_bytes_read - 1;
+                        self.result.version_len += 1;
                         self.state = .on_version;
                     } else {
-                        self.total_bytes_read += pos;
                         return error.BadRequest;
                     }
                 },
                 .on_version => {
                     if (c == '\r') {
-                        self.version_len += pos - 1 - version_start_pos;
                         self.state = .seen_cr;
-                    } else if (self.version_len + pos - version_start_pos > version_max_len) {
-                        self.total_bytes_read += pos;
-                        return error.BadRequest;
+                    } else {
+                        self.result.version_len += 1;
+                        if (self.result.version_len > version_max_len) {
+                            return error.BadRequest;
+                        }
                     }
                 },
                 .seen_cr => {
-                    self.total_bytes_read += pos;
-                    return if (c == '\n') true else error.BadRequest;
+                    if (c == '\n') {
+                        self.state = .done;
+                        return true;
+                    }
+                    return error.BadRequest;
                 },
                 .done => return error.InvalidState,
             }
         }
-        self.total_bytes_read += chunk.len;
-        switch (self.state) {
-            .on_uri => self.uri_len += pos - uri_start_pos,
-            .on_version => self.version_len += pos - version_start_pos,
-            else => {},
-        }
         return false;
-    }
-
-    pub fn totalBytesRead(self: *const RequestLineParser) usize {
-        return self.total_bytes_read;
     }
 };
 
@@ -150,39 +153,39 @@ const FieldSectionEndFinder = struct {
                 .initial => {
                     if (std.mem.indexOfScalarPos(u8, buf, pos, '\r')) |cr_pos| {
                         self.state = .seen_cr;
+                        self.total_bytes_read += cr_pos + "\r".len - pos;
                         pos = cr_pos + "\r".len;
                     } else {
-                        self.total_bytes_read += buf.len;
+                        self.total_bytes_read += buf.len - pos;
                         return false;
                     }
                 },
                 .seen_cr => {
+                    self.total_bytes_read += 1;
                     if (buf[pos] == '\n') {
                         self.state = .seen_cr_lf;
                         pos += 1;
                     } else {
-                        self.total_bytes_read += pos + 1;
                         return error.InvalidInput;
                     }
                 },
                 .seen_cr_lf => {
+                    self.total_bytes_read += 1;
                     self.state = if (buf[pos] == '\r') .seen_cr_lf_cr else .initial;
                     pos += 1;
                 },
                 .seen_cr_lf_cr => {
+                    self.total_bytes_read += 1;
                     if (buf[pos] == '\n') {
                         self.state = .seen_cr_lf_cr_lf;
-                        self.total_bytes_read += pos + 1;
                         return true;
                     } else {
-                        self.total_bytes_read += pos + 1;
                         return error.InvalidInput;
                     }
                 },
                 .seen_cr_lf_cr_lf => return error.InvalidState,
             }
         }
-        self.total_bytes_read += buf.len;
         return false;
     }
 
@@ -194,58 +197,99 @@ const FieldSectionEndFinder = struct {
 const testing = std.testing;
 
 test "RcvRequest - GET method" {
+    const method = "GET";
+    const uri = "http://www.example.org/where?q=now";
+    const version = "HTTP/1.1";
+    const headers = "Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n" ++
+        "Server: Apache\r\n" ++
+        "\r\n";
+    const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n" ++ headers;
+
+    var splitter = RequestLineSplitter{};
+    try testing.expect(try splitter.parse(input));
+    var finder = FieldSectionEndFinder{};
+    try testing.expect(try finder.reachesToEnd(input[splitter.result.total_bytes_read..]));
+    try testing.expectEqual(input.len, splitter.result.total_bytes_read + finder.total_bytes_read);
+
     const allocator = testing.allocator;
-    const method = Method{ .get = undefined };
-    const uri = try allocator.dupe(u8, "http://www.example.org/where?q=now");
-    const headers = try allocator.dupe(u8, "Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n" ++
-        "Server: Apache\r\n");
-    var req = RecvRequest.init(allocator, method, uri, Version.http1_1, headers);
+    const buf = try allocator.dupe(u8, input);
+    var req = try RecvRequest.init(allocator, buf, &splitter.result, finder.total_bytes_read);
     defer req.deinit();
+
+    try testing.expectEqual(Method{ .get = undefined }, req.method);
+    try testing.expectEqualStrings(uri, req.uri);
+    try testing.expectEqual(try Version.fromText(version), req.version);
+    try testing.expectEqualStrings(headers, req.headers);
 }
 
 test "RcvRequest - custom method" {
+    const method = "PURGE_ALL";
+    const uri = "http://www.example.org/where?q=now";
+    const version = "HTTP/1.1";
+    const headers = "Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n" ++
+        "Server: Apache\r\n" ++
+        "\r\n";
+    const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n" ++ headers;
+
+    var splitter = RequestLineSplitter{};
+    try testing.expect(try splitter.parse(input));
+    var finder = FieldSectionEndFinder{};
+    try testing.expect(try finder.reachesToEnd(input[splitter.result.total_bytes_read..]));
+    try testing.expectEqual(input.len, splitter.result.total_bytes_read + finder.total_bytes_read);
+
     const allocator = testing.allocator;
-    const method = Method{ .custom = try allocator.dupe(u8, "PURGE_ALL") };
-    const uri = try allocator.dupe(u8, "http://www.example.org/where?q=now");
-    const headers = try allocator.dupe(u8, "Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n" ++
-        "Server: Apache\r\n");
-    var req = RecvRequest.init(allocator, method, uri, Version.http1_1, headers);
+    const buf = try allocator.dupe(u8, input);
+    var req = try RecvRequest.init(allocator, buf, &splitter.result, finder.total_bytes_read);
     defer req.deinit();
+
+    switch (req.method) {
+        .custom => |v| try testing.expectEqualStrings(method, v),
+        else => unreachable,
+    }
+    try testing.expectEqualStrings(uri, req.uri);
+    try testing.expectEqual(try Version.fromText(version), req.version);
+    try testing.expectEqualStrings(headers, req.headers);
 }
 
-test "RequestLineParser - whole in one buf" {
+test "RequestLineSplitter - whole in one buf" {
     const method = "GET";
     const uri = "http://www.example.org/where?q=now";
     const version = "HTTP/1.1";
     const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n";
 
-    var parser = RequestLineParser{};
-    try testing.expect(try parser.parse(input));
-    try testing.expectEqual(method.len, parser.method_len);
-    try testing.expectEqual(uri.len, parser.uri_len);
-    try testing.expectEqual(version.len, parser.version_len);
-    try testing.expectEqual(input.len, parser.totalBytesRead());
+    var splitter = RequestLineSplitter{};
+    try testing.expect(try splitter.parse(input));
+    const result = splitter.result;
+    try testing.expectEqual(method.len, result.method_len);
+    try testing.expectEqual(method.len + 1, result.uri_start_pos);
+    try testing.expectEqual(uri.len, result.uri_len);
+    try testing.expectEqual(method.len + 1 + uri.len + 1, result.version_start_pos);
+    try testing.expectEqual(version.len, result.version_len);
+    try testing.expectEqual(input.len, result.total_bytes_read);
 }
 
-test "RequestLineParser - one byte at time" {
+test "RequestLineSplitter - one byte at time" {
     const method = "GET";
     const uri = "http://www.example.org/where?q=now";
     const version = "HTTP/1.1";
     const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n";
 
-    var parser = RequestLineParser{};
+    var splitter = RequestLineSplitter{};
     var i: usize = 0;
     while (i < input.len - 2) : (i += 1) {
-        try testing.expect(!try parser.parse(input[i .. i + 1]));
+        try testing.expect(!try splitter.parse(input[i .. i + 1]));
     }
-    try testing.expect(try parser.parse(input[i..]));
-    try testing.expectEqual(method.len, parser.method_len);
-    try testing.expectEqual(uri.len, parser.uri_len);
-    try testing.expectEqual(version.len, parser.version_len);
-    try testing.expectEqual(input.len, parser.totalBytesRead());
+    try testing.expect(try splitter.parse(input[i..]));
+    const result = splitter.result;
+    try testing.expectEqual(method.len, result.method_len);
+    try testing.expectEqual(method.len + 1, result.uri_start_pos);
+    try testing.expectEqual(uri.len, result.uri_len);
+    try testing.expectEqual(method.len + 1 + uri.len + 1, result.version_start_pos);
+    try testing.expectEqual(version.len, result.version_len);
+    try testing.expectEqual(input.len, result.total_bytes_read);
 }
 
-test "RequestLineParser - variable length chunks" {
+test "RequestLineSplitter - variable length chunks" {
     const method = "GET";
     const uri = "http://www.example.org/where?q=now";
     const version = "HTTP/1.1";
@@ -257,52 +301,57 @@ test "RequestLineParser - variable length chunks" {
         method.len + " ".len + uri.len + " ".len + version.len - 1,
         input.len,
     };
-    var parser = RequestLineParser{};
+    var splitter = RequestLineSplitter{};
 
     var start: usize = 0;
     for (ends) |end| {
-        try testing.expect((try parser.parse(input[start..end])) == (end == input.len));
+        try testing.expect((try splitter.parse(input[start..end])) == (end == input.len));
         start = end;
     }
-    try testing.expectEqual(method.len, parser.method_len);
-    try testing.expectEqual(uri.len, parser.uri_len);
-    try testing.expectEqual(version.len, parser.version_len);
-    try testing.expectEqual(input.len, parser.totalBytesRead());
+    const result = splitter.result;
+    try testing.expectEqual(method.len, result.method_len);
+    try testing.expectEqual(method.len + 1, result.uri_start_pos);
+    try testing.expectEqual(uri.len, result.uri_len);
+    try testing.expectEqual(method.len + 1 + uri.len + 1, result.version_start_pos);
+    try testing.expectEqual(version.len, result.version_len);
+    try testing.expectEqual(input.len, result.total_bytes_read);
 }
 
-test "RequestLineParser - method too long" {
+test "RequestLineSplitter - method too long" {
     const method = "PURGE_ALL";
     const uri = "http://www.example.org/where?q=now";
     const version = "HTTP/1.1";
     const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n";
 
     const method_max_len_test = 7;
-    var parser = RequestLineParser{ .method_max_len = method_max_len_test };
-    try testing.expectError(error.BadRequest, parser.parse(input));
-    try testing.expectEqual(@as(usize, method_max_len_test + 1), parser.totalBytesRead());
+    var splitter = RequestLineSplitter{ .method_max_len = method_max_len_test };
+    try testing.expectError(error.BadRequest, splitter.parse(input));
+    try testing.expectEqual(@as(usize, method_max_len_test + 1), splitter.result.total_bytes_read);
 }
 
-test "RequestLineParser - URI too long" {
+test "RequestLineSplitter - URI too long" {
     const method = "GET";
     const uri = "http://www.example.org/where?q=now";
     const version = "HTTP/1.1";
     const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n";
 
     const uri_max_len_test = 12;
-    var parser = RequestLineParser{ .uri_max_len = uri_max_len_test };
-    try testing.expectError(error.UriTooLong, parser.parse(input));
-    try testing.expectEqual(@as(usize, method.len + " ".len + uri_max_len_test + 1), parser.totalBytesRead());
+    var splitter = RequestLineSplitter{ .uri_max_len = uri_max_len_test };
+    try testing.expectError(error.UriTooLong, splitter.parse(input));
+    const expected_total_len = method.len + " ".len + uri_max_len_test + 1;
+    try testing.expectEqual(expected_total_len, splitter.result.total_bytes_read);
 }
 
-test "RequestLineParser - version too long" {
+test "RequestLineSplitter - version too long" {
     const method = "GET";
     const uri = "http://www.example.org/where?q=now";
     const version = "HTTP/3.14";
     const input = method ++ " " ++ uri ++ " " ++ version ++ "\r\n";
 
-    var parser = RequestLineParser{};
-    try testing.expectError(error.BadRequest, parser.parse(input));
-    try testing.expectEqual(@as(usize, method.len + " ".len + uri.len + " ".len + version_max_len + 1), parser.totalBytesRead());
+    var splitter = RequestLineSplitter{};
+    try testing.expectError(error.BadRequest, splitter.parse(input));
+    const expected_total_len = method.len + " ".len + uri.len + " ".len + version_max_len + 1;
+    try testing.expectEqual(expected_total_len, splitter.result.total_bytes_read);
 }
 
 test "FieldSectionEndFinder - whole in one buf" {
--- src/version.zig
+++ src/version.zig
@@ -7,7 +7,7 @@ pub const Version = enum {
     http2,
     http3,
 
-    pub fn fromText(input: []const u8) ?Version {
+    pub fn fromText(input: []const u8) !Version {
         if (std.mem.startsWith(u8, input, "HTTP/")) {
             const v: []const u8 = input["HTTP/".len..];
             if (v.len == 1) {
@@ -20,7 +20,7 @@ pub const Version = enum {
                 } else if (v[0] == '0' and v[2] == '9') return .http0_9;
             }
         }
-        return null;
+        return error.BadVersion;
     }
 
     pub fn toText(self: Version) []const u8 {
@@ -30,17 +30,17 @@ pub const Version = enum {
             .http1_1 => "HTTP/1.1",
             .http2 => "HTTP/2",
             .http3 => "HTTP/3",
-        };        
+        };
     }
 };
 
 const testing = std.testing;
 
 test "http.Version" {
-    try testing.expectEqual(Version.http0_9, Version.fromText("HTTP/0.9").?);
-    try testing.expectEqual(Version.http1_0, Version.fromText("HTTP/1.0").?);
-    try testing.expectEqual(Version.http1_1, Version.fromText("HTTP/1.1").?);
-    try testing.expectEqual(Version.http2, Version.fromText("HTTP/2").?);
-    try testing.expectEqual(Version.http3, Version.fromText("HTTP/3").?);
-    try testing.expect(Version.fromText("HTTP/1.1 ") == null);
+    try testing.expectEqual(Version.http0_9, try Version.fromText("HTTP/0.9"));
+    try testing.expectEqual(Version.http1_0, try Version.fromText("HTTP/1.0"));
+    try testing.expectEqual(Version.http1_1, try Version.fromText("HTTP/1.1"));
+    try testing.expectEqual(Version.http2, try Version.fromText("HTTP/2"));
+    try testing.expectEqual(Version.http3, try Version.fromText("HTTP/3"));
+    try testing.expectError(error.BadVersion, Version.fromText("HTTP/1.1 "));
 }
