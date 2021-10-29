d97512efef148e0facb318e47e9ba8fba18fdae5
Hiroaki Nakamura <hnakamur@gmail.com>
2021-10-29 19:38:55 +0900

    Add toy http_server

--- build.zig
+++ build.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
+const pkgs = @import("deps.zig").pkgs;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
     const lib = b.addStaticLibrary("http", "src/main.zig");
     lib.setBuildMode(mode);
@@ -14,4 +16,16 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    const example_step = b.step("examples", "Build examples");
+    inline for (.{
+        "http_server",
+    }) |example_name| {
+        const example = b.addExecutable(example_name, "examples/" ++ example_name ++ ".zig");
+        example.setBuildMode(mode);
+        example.setTarget(target);
+        pkgs.addAllTo(example);
+        example.install();
+        example_step.dependOn(&example.step);
+    }
 }
