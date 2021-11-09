63887cb98a6696093251113aa7702aad68f420c0
Hiroaki Nakamura <hnakamur@gmail.com>
2021-11-09 21:50:10 +0900

    Add sendWithTimeout and recvWithTimeout

--- examples/tcp_conn.zig
+++ examples/tcp_conn.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const time = std.time;
 const IO = @import("tigerbeetle-io").IO;
+const http = @import("http");
 const TCPConn = @import("http").TCPConn;
 
 pub fn main() !void {
@@ -17,6 +18,12 @@ pub fn main() !void {
     const MyContext = struct {
         conn: TCPConn,
         completion: TCPConn.Completion = undefined,
+        send_buf: [1024]u8 = [_]u8{0} ** 1024,
+        recv_buf: [1024]u8 = [_]u8{0} ** 1024,
+        connect_timeout: u63 = 500 * time.ns_per_ms,
+        send_timeout: u63 = 500 * time.ns_per_ms,
+        recv_timeout: u63 = 500 * time.ns_per_ms,
+        done: bool = false,
 
         const Self = @This();
 
@@ -26,7 +33,7 @@ pub fn main() !void {
                 self,
                 connectCallback,
                 &self.completion,
-                500 * time.ns_per_ms,
+                self.connect_timeout,
             );
         }
         fn connectCallback(
@@ -35,10 +42,57 @@ pub fn main() !void {
             result: TCPConn.ConnectError!void,
         ) void {
             std.debug.print("MyContext.connectCallback, result={}\n", .{result});
+
+            var fbs = std.io.fixedBufferStream(&self.send_buf);
+            var w = fbs.writer();
+            std.fmt.format(w, "{s} {s} {s}\r\n", .{
+                (http.Method{ .get = undefined }).toText(),
+                "/",
+                http.Version.http1_1.toText(),
+            }) catch unreachable;
+            std.fmt.format(w, "Host: example.com\r\n\r\n", .{}) catch unreachable;
+            self.conn.sendWithTimeout(
+                *Self,
+                self,
+                sendCallback,
+                &self.completion,
+                fbs.getWritten(),
+                self.send_timeout,
+            );
+        }
+        fn sendCallback(
+            self: *Self,
+            comp: *TCPConn.Completion,
+            result: TCPConn.SendError!usize,
+        ) void {
+            std.debug.print("MyContext.sendCallback, result={}\n", .{result});
+            self.conn.recvWithTimeout(
+                *Self,
+                self,
+                recvCallback,
+                &self.completion,
+                &self.recv_buf,
+                self.recv_timeout,
+            );
+        }
+        fn recvCallback(
+            self: *Self,
+            comp: *TCPConn.Completion,
+            result: TCPConn.RecvError!usize,
+        ) void {
+            std.debug.print("MyContext.recvCallback, result={}\n", .{result});
+            if (result) |received| {
+                std.debug.print("response={s}", .{self.recv_buf[0..received]});
+            } else |err| {
+                std.debug.print("MyContext.recvCallback, err={s}\n", .{@errorName(err)});
+            }
+            self.done = true;
         }
     };
+
     var ctx = MyContext{ .conn = conn };
     ctx.connect();
-
-    try io.run_for_ns(2 * time.ns_per_s);
+    while (!ctx.done) {
+        try io.run_for_ns(time.ns_per_s);
+    }
 }
--- src/tcp_conn.zig
+++ src/tcp_conn.zig
@@ -10,16 +10,18 @@ pub const TCPConn = struct {
     address: std.net.Address,
     socket: os.socket_t,
 
-    const Result = union(enum) {
-        connect: ConnectError!void,
-    };
-
     pub const Completion = struct {
         context: ?*c_void,
         callback: fn (ctx: ?*c_void, comp: *Completion, res: Result) void = undefined,
+        result: Result = undefined,
         completion1: IO.Completion = undefined,
         completion2: IO.Completion = undefined,
-        result: Result = undefined,
+    };
+
+    const Result = union(enum) {
+        connect: ConnectError!void,
+        send: SendError!usize,
+        recv: RecvError!usize,
     };
 
     const Self = @This();
@@ -149,198 +151,231 @@ pub const TCPConn = struct {
         std.debug.print("connectTimeoutCancelCallback called callback\n", .{});
     }
 
-    // pub const SendError = IO.SendError || IO.TimeoutError;
+    pub const SendError = IO.SendError;
 
-    // pub fn send(
-    //     self: *Self,
-    //     comptime Context: type,
-    //     context: Context,
-    //     sock: os.socket_t,
-    //     buf: []const u8,
-    //     callback: fn (
-    //         context: Context,
-    //         client: *Self,
-    //         result: SendError!usize,
-    //     ) void,
-    // ) void {
-    //     std.debug.print("Self.send start\n", .{});
-    //     callback(context, self, 1);
-    //     std.debug.print("Self.send exit\n", .{});
-    // }
+    pub fn sendWithTimeout(
+        self: *Self,
+        comptime Context: type,
+        context: Context,
+        comptime callback: fn (
+            context: Context,
+            completion: *Completion,
+            result: SendError!usize,
+        ) void,
+        completion: *Completion,
+        buf: []const u8,
+        timeout_ns: u63,
+    ) void {
+        completion.context = context;
+        completion.callback = struct {
+            fn wrapper(ctx: ?*c_void, comp: *Completion, res: Result) void {
+                callback(
+                    @intToPtr(Context, @ptrToInt(ctx)),
+                    comp,
+                    res.send,
+                );
+            }
+        }.wrapper;
+        self.io.send(
+            *Self,
+            self,
+            sendCallback,
+            &completion.completion1,
+            self.socket,
+            buf,
+            if (std.Target.current.os.tag == .linux) os.MSG_NOSIGNAL else 0,
+        );
+        self.io.timeout(
+            *Self,
+            self,
+            sendTimeoutCallback,
+            &completion.completion2,
+            timeout_ns,
+        );
+    }
+    fn sendCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.SendError!usize,
+    ) void {
+        std.debug.print("TCPConn.sendCallback result={}\n", .{result});
+        var completion = @fieldParentPtr(Completion, "completion1", io_completion);
+        completion.result = .{ .send = result };
+        if (result) |sent| {
+            self.io.cancelTimeout(
+                *Self,
+                self,
+                sendTimeoutCancelCallback,
+                &completion.completion1,
+                &completion.completion2,
+            );
+        } else |err| {
+            std.debug.print("send error: {s}\n", .{@errorName(err)});
+        }
+    }
+    fn sendTimeoutCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.TimeoutError!void,
+    ) void {
+        if (result) |_| {
+            std.debug.print("sendTimeoutCallback ok\n", .{});
+            var completion = @fieldParentPtr(Completion, "completion2", io_completion);
+            self.io.cancel(
+                *Self,
+                self,
+                sendCancelCallback,
+                &completion.completion2,
+                &completion.completion1,
+            );
+        } else |err| {
+            if (err != error.Canceled) {
+                std.debug.print("sendTimeoutCallback err={s}\n", .{@errorName(err)});
+            }
+        }
+    }
+    fn sendCancelCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.CancelError!void,
+    ) void {
+        if (result) |_| {
+            std.debug.print("sendCancelCallback ok\n", .{});
+        } else |err| {
+            std.debug.print("sendCancelCallback err={s}\n", .{@errorName(err)});
+        }
+        var completion = @fieldParentPtr(Completion, "completion2", io_completion);
+        std.debug.print("TCPConn.sendCancelCallback calling callback, result={}\n", .{completion.result});
+        completion.callback(completion.context, completion, completion.result);
+        std.debug.print("TCPConn.sendCancelCallback called callback\n", .{});
+    }
+    fn sendTimeoutCancelCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.CancelTimeoutError!void,
+    ) void {
+        if (result) |_| {
+            std.debug.print("sendTimeoutCancelCallback ok\n", .{});
+        } else |err| {
+            std.debug.print("sendTimeoutCancelCallback err={s}\n", .{@errorName(err)});
+        }
+        var completion = @fieldParentPtr(Completion, "completion1", io_completion);
+        std.debug.print("TCPConn.sendTimeoutCancelCallback calling callback, result={}\n", .{completion.result});
+        completion.callback(completion.context, completion, completion.result);
+        std.debug.print("TCPConn.sendTimeoutCancelCallback called callback\n", .{});
+    }
 
-    // fn _sendWithTimeout(self: *Self) void {
-    //     var fbs = std.io.fixedBufferStream(self.send_buf);
-    //     var w = fbs.writer();
-    //     std.fmt.format(w, "{s} {s} {s}\r\n", .{
-    //         (Method{ .get = undefined }).toText(),
-    //         "/",
-    //         Version.http1_1.toText(),
-    //     }) catch unreachable;
-    //     std.fmt.format(w, "Host: example.com\r\n\r\n", .{}) catch unreachable;
+    pub const RecvError = IO.RecvError;
 
-    //     self.io.send(
-    //         *Self,
-    //         self,
-    //         sendCallback,
-    //         &self.completions[0],
-    //         self.sock,
-    //         fbs.getWritten(),
-    //         if (std.Target.current.os.tag == .linux) os.MSG_NOSIGNAL else 0,
-    //     );
-    //     self.io.timeout(
-    //         *Self,
-    //         self,
-    //         sendTimeoutCallback,
-    //         &self.completions[1],
-    //         self.send_timeout_ns,
-    //     );
-    // }
-    // fn sendCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.SendError!usize,
-    // ) void {
-    //     if (result) |sent| {
-    //         std.debug.print("sent request bytes={d}\n", .{sent});
-    //         self.io.cancelTimeout(
-    //             *Self,
-    //             self,
-    //             sendTimeoutCancelCallback,
-    //             &self.completions[0],
-    //             &self.completions[1],
-    //         );
-    //     } else |err| {
-    //         std.debug.print("send error: {s}\n", .{@errorName(err)});
-    //         self.close();
-    //     }
-    // }
-    // fn sendTimeoutCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.TimeoutError!void,
-    // ) void {
-    //     if (result) |_| {
-    //         std.debug.print("sendTimeoutCallback ok\n", .{});
-    //         completion.io.cancel(
-    //             *Self,
-    //             self,
-    //             sendCancelCallback,
-    //             &self.completions[1],
-    //             &self.completions[0],
-    //         );
-    //     } else |err| {
-    //         if (err != error.Canceled) {
-    //             std.debug.print("sendTimeoutCallback err={s}\n", .{@errorName(err)});
-    //         }
-    //     }
-    // }
-    // fn sendCancelCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.CancelError!void,
-    // ) void {
-    //     if (result) |_| {
-    //         std.debug.print("sendCancelCallback ok\n", .{});
-    //     } else |err| {
-    //         std.debug.print("sendCancelCallback err={s}\n", .{@errorName(err)});
-    //     }
-    // }
-    // fn sendTimeoutCancelCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.CancelTimeoutError!void,
-    // ) void {
-    //     if (result) |_| {
-    //         std.debug.print("sendTimeoutCancelCallback ok\n", .{});
-    //         self.recvWithTimeout();
-    //     } else |err| {
-    //         std.debug.print("sendTimeoutCancelCallback err={s}\n", .{@errorName(err)});
-    //     }
-    // }
-
-    // fn recvWithTimeout(self: *Self) void {
-    //     self.io.recv(
-    //         *Self,
-    //         self,
-    //         recvCallback,
-    //         &self.completions[0],
-    //         self.sock,
-    //         self.recv_buf,
-    //         if (std.Target.current.os.tag == .linux) os.MSG_NOSIGNAL else 0,
-    //     );
-    //     self.io.timeout(
-    //         *Self,
-    //         self,
-    //         recvTimeoutCallback,
-    //         &self.completions[1],
-    //         self.recv_timeout_ns,
-    //     );
-    // }
-    // fn recvCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.RecvError!usize,
-    // ) void {
-    //     if (result) |received| {
-    //         std.debug.print("response={s}", .{self.recv_buf[0..received]});
-    //         self.io.cancelTimeout(
-    //             *Self,
-    //             self,
-    //             recvTimeoutCancelCallback,
-    //             &self.completions[0],
-    //             &self.completions[1],
-    //         );
-    //     } else |err| {
-    //         std.debug.print("recv error: {s}\n", .{@errorName(err)});
-    //     }
-    // }
-    // fn recvTimeoutCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.TimeoutError!void,
-    // ) void {
-    //     if (result) |_| {
-    //         std.debug.print("recvTimeoutCallback ok\n", .{});
-    //         completion.io.cancel(
-    //             *Self,
-    //             self,
-    //             recvCancelCallback,
-    //             &self.completions[1],
-    //             &self.completions[0],
-    //         );
-    //     } else |err| {
-    //         std.debug.print("recvTimeoutCallback err={s}\n", .{@errorName(err)});
-    //     }
-    // }
-    // fn recvCancelCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.CancelError!void,
-    // ) void {
-    //     if (result) |_| {
-    //         std.debug.print("recvCancelCallback ok\n", .{});
-    //     } else |err| {
-    //         std.debug.print("recvCancelCallback err={s}\n", .{@errorName(err)});
-    //     }
-    // }
-    // fn recvTimeoutCancelCallback(
-    //     self: *Self,
-    //     completion: *IO.Completion,
-    //     result: IO.CancelTimeoutError!void,
-    // ) void {
-    //     if (result) |_| {
-    //         std.debug.print("recvTimeoutCancelCallback ok\n", .{});
-    //     } else |err| {
-    //         std.debug.print("recvTimeoutCancelCallback error: {s}\n", .{@errorName(err)});
-    //     }
-    //     self.close();
-    // }
-
-    // fn close(self: *Self) void {
-    //     os.close(self.sock);
-    //     self.done = true;
-    //     std.debug.print("close and exit\n", .{});
-    // }
+    pub fn recvWithTimeout(
+        self: *Self,
+        comptime Context: type,
+        context: Context,
+        comptime callback: fn (
+            context: Context,
+            completion: *Completion,
+            result: RecvError!usize,
+        ) void,
+        completion: *Completion,
+        buf: []u8,
+        timeout_ns: u63,
+    ) void {
+        completion.context = context;
+        completion.callback = struct {
+            fn wrapper(ctx: ?*c_void, comp: *Completion, res: Result) void {
+                callback(
+                    @intToPtr(Context, @ptrToInt(ctx)),
+                    comp,
+                    res.recv,
+                );
+            }
+        }.wrapper;
+        self.io.recv(
+            *Self,
+            self,
+            recvCallback,
+            &completion.completion1,
+            self.socket,
+            buf,
+            if (std.Target.current.os.tag == .linux) os.MSG_NOSIGNAL else 0,
+        );
+        self.io.timeout(
+            *Self,
+            self,
+            recvTimeoutCallback,
+            &completion.completion2,
+            timeout_ns,
+        );
+    }
+    fn recvCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.RecvError!usize,
+    ) void {
+        if (result) |received| {
+            std.debug.print("TCPConn.recvCallback result={}", .{result});
+            var completion = @fieldParentPtr(Completion, "completion1", io_completion);
+            completion.result = .{ .recv = result };
+            self.io.cancelTimeout(
+                *Self,
+                self,
+                recvTimeoutCancelCallback,
+                &completion.completion1,
+                &completion.completion2,
+            );
+        } else |err| {
+            std.debug.print("recv error: {s}\n", .{@errorName(err)});
+        }
+    }
+    fn recvTimeoutCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.TimeoutError!void,
+    ) void {
+        if (result) |_| {
+            std.debug.print("recvTimeoutCallback ok\n", .{});
+            var completion = @fieldParentPtr(Completion, "completion2", io_completion);
+            self.io.cancel(
+                *Self,
+                self,
+                recvCancelCallback,
+                &completion.completion2,
+                &completion.completion1,
+            );
+        } else |err| {
+            std.debug.print("recvTimeoutCallback err={s}\n", .{@errorName(err)});
+        }
+    }
+    fn recvCancelCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.CancelError!void,
+    ) void {
+        if (result) |_| {
+            std.debug.print("recvCancelCallback ok\n", .{});
+        } else |err| {
+            std.debug.print("recvCancelCallback err={s}\n", .{@errorName(err)});
+        }
+        var completion = @fieldParentPtr(Completion, "completion2", io_completion);
+        std.debug.print("TCPConn.recvCancelCallback calling callback, result={}\n", .{completion.result});
+        completion.callback(completion.context, completion, completion.result);
+        std.debug.print("TCPConn.recvCancelCallback called callback\n", .{});
+    }
+    fn recvTimeoutCancelCallback(
+        self: *Self,
+        io_completion: *IO.Completion,
+        result: IO.CancelTimeoutError!void,
+    ) void {
+        if (result) |_| {
+            std.debug.print("recvTimeoutCancelCallback ok\n", .{});
+        } else |err| {
+            std.debug.print("recvTimeoutCancelCallback error: {s}\n", .{@errorName(err)});
+        }
+        var completion = @fieldParentPtr(Completion, "completion1", io_completion);
+        std.debug.print("TCPConn.recvTimeoutCancelCallback calling callback, result={}\n", .{completion.result});
+        completion.callback(completion.context, completion, completion.result);
+        std.debug.print("TCPConn.recvTimeoutCancelCallback called callback\n", .{});
+    }
 };
 
 const testing = std.testing;
