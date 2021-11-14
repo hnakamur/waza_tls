39b64d5406966d8097ada0e7eaddc599be75054e
Hiroaki Nakamura <hnakamur@gmail.com>
2021-11-14 09:45:15 +0900

    Delete zigtest.sh script

--- zigtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-zig test \
- --pkg-begin http ./src/main.zig \
-  --pkg-begin tigerbeetle-io ./lib/tigerbeetle-io/src/main.zig --pkg-end \
- --pkg-end \
- --pkg-begin tigerbeetle-io ./lib/tigerbeetle-io/src/main.zig --pkg-end \
- --pkg-begin datetime ./lib/zig-datetime/src/main.zig --pkg-end \
- "$@"
