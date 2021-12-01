8ca59c234801c0a0806fc44b59f93ee071e3632e
Hiroaki Nakamura <hnakamur@gmail.com>
2021-12-01 23:37:17 +0900

    Add bad_req_content_len test

--- tests/real/error/main.zig
+++ tests/real/error/main.zig
@@ -4,6 +4,7 @@ const client_recv_timeout = @import("client_recv_timeout.zig");
 const connection_refused = @import("connection_refused.zig");
 const connection_timedout = @import("connection_timedout.zig");
 
+const bad_req_content_len = @import("bad_req_content_len.zig");
 const drop_server_recv = @import("drop_server_recv.zig");
 const http09_unsupported = @import("http09_unsupported.zig");
 const http2_unsupported = @import("http2_unsupported.zig");
