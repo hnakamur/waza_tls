8b63ae956bdc5b422695cae2f21c4a6828dafded
Hiroaki Nakamura <hnakamur@gmail.com>
2021-12-01 22:51:25 +0900

    Add too_long_req_hdr test

--- tests/real/error/main.zig
+++ tests/real/error/main.zig
@@ -5,6 +5,7 @@ const connection_refused = @import("connection_refused.zig");
 const connection_timedout = @import("connection_timedout.zig");
 
 const drop_server_recv = @import("drop_server_recv.zig");
+const too_long_req_hdr = @import("too_long_req_hdr.zig");
 
 comptime {
     std.testing.refAllDecls(@This());
