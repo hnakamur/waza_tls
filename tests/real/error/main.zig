d30822313c3cb532946edf2b7af4d5939f7a9974
Hiroaki Nakamura <hnakamur@gmail.com>
2021-12-01 23:15:12 +0900

    Add too_long_req_method test

--- tests/real/error/main.zig
+++ tests/real/error/main.zig
@@ -6,6 +6,7 @@ const connection_timedout = @import("connection_timedout.zig");
 
 const drop_server_recv = @import("drop_server_recv.zig");
 const too_long_req_hdr = @import("too_long_req_hdr.zig");
+const too_long_req_method = @import("too_long_req_method.zig");
 const too_long_req_uri = @import("too_long_req_uri.zig");
 
 comptime {
