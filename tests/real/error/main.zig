42cbe1f094194b4afe94a66f766340157cb17493
Hiroaki Nakamura <hnakamur@gmail.com>
2021-12-01 23:20:33 +0900

    Add http09_unsupported test

--- tests/real/error/main.zig
+++ tests/real/error/main.zig
@@ -5,6 +5,7 @@ const connection_refused = @import("connection_refused.zig");
 const connection_timedout = @import("connection_timedout.zig");
 
 const drop_server_recv = @import("drop_server_recv.zig");
+const http09_unsupported = @import("http09_unsupported.zig");
 const too_long_req_hdr = @import("too_long_req_hdr.zig");
 const too_long_req_method = @import("too_long_req_method.zig");
 const too_long_req_uri = @import("too_long_req_uri.zig");
