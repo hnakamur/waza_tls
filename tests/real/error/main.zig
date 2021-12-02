276ae1581a229e2017705ac8d2604e21d7ffa8b5
Hiroaki Nakamura <hnakamur@gmail.com>
2021-12-02 23:10:20 +0900

    Add bad_resp_status test

--- tests/real/error/main.zig
+++ tests/real/error/main.zig
@@ -8,6 +8,7 @@ const bad_req_content_len = @import("bad_req_content_len.zig");
 const bad_req_http_version = @import("bad_req_http_version.zig");
 const bad_resp_content_len = @import("bad_resp_content_len.zig");
 const bad_resp_http_version = @import("bad_resp_http_version.zig");
+const bad_resp_status = @import("bad_resp_status.zig");
 const drop_server_recv = @import("drop_server_recv.zig");
 const http09_unsupported = @import("http09_unsupported.zig");
 const http2_unsupported = @import("http2_unsupported.zig");
