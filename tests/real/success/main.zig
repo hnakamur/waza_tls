2e2a6ebbaab969b518fe97b0a476f96b7c9de443
Hiroaki Nakamura <hnakamur@gmail.com>
2021-12-05 10:39:24 +0900

    Add proxy_two_reqs

--- tests/real/success/main.zig
+++ tests/real/success/main.zig
@@ -7,6 +7,7 @@ const reuse_conn_slot = @import("reuse_conn_slot.zig");
 const simple_get = @import("simple_get.zig");
 
 const proxy_simple_get = @import("proxy_simple_get.zig");
+const proxy_two_reqs = @import("proxy_two_reqs.zig");
 
 comptime {
     std.testing.refAllDecls(@This());
