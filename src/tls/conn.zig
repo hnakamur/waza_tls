396fce34482e8a49298f50b0a3909322d05fb5ea
Hiroaki Nakamura <hnakamur@gmail.com>
2022-01-07 23:36:19 +0900

    Add Conn.writeRecord

--- src/tls/conn.zig
+++ src/tls/conn.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const fifo = std.fifo;
+const math = std.math;
 const mem = std.mem;
 const net = std.net;
 const CipherSuite = @import("cipher_suites.zig").CipherSuite;
@@ -7,21 +8,33 @@ const ProtocolVersion = @import("handshake_msg.zig").ProtocolVersion;
 const RecordType = @import("record.zig").RecordType;
 
 const max_plain_text = 16384; // maximum plaintext payload length
+const record_header_len = 5;
 
 pub const Conn = struct {
     stream: net.Stream,
     in: HalfConn,
     out: HalfConn,
     version: ?ProtocolVersion = null,
+    buffering: bool = false,
+    send_buf: std.ArrayListUnmanaged(u8) = .{},
+    bytes_sent: usize = 0,
 
-    pub fn writeRecord(self: *Conn, record_type: RecordType, data: []const u8) !void {
-        _ = record_type;
-        _ = data;
+    pub fn writeRecord(
+        self: *Conn,
+        allocator: mem.Allocator,
+        record_type: RecordType,
+        data: []const u8,
+    ) !void {
+        var out_buf = try std.ArrayListUnmanaged(u8).initCapacity(allocator, record_header_len);
+        defer out_buf.deinit(allocator);
+
         var n: usize = 0;
-        _ = n;
         var rest = data;
         while (rest.len > 0) {
-            const m = data.len;
+            out_buf.clearRetainingCapacity();
+            var writer = out_buf.writer();
+            try writer.writeByte(@enumToInt(record_type));
+
             const vers = if (self.version) |vers| blk: {
                 // TLS 1.3 froze the record layer version to 1.2.
                 // See RFC 8446, Section 5.1.
@@ -31,16 +44,49 @@ pub const Conn = struct {
                 // greater than TLS 1.0 for the initial ClientHello.
                 break :blk .v1_0;
             };
-            _ = m;
-            _ = vers;
+            try writer.writeIntBig(u16, @enumToInt(vers));
+
+            const m = math.min(rest.len, self.maxPayloadSizeForWrite(record_type));
+            try writer.writeIntBig(u16, @intCast(u16, m));
+
+            try self.out.encrypt(allocator, &out_buf, rest[0..m]);
+            try self.write(allocator, out_buf.items);
+            n += m;
+            rest = rest[m..];
+        }
+
+        if (record_type == .change_cipher_spec and self.version != .v1_3) {
+            // TODO: implement
         }
     }
 
-    fn maxPayloadSizeForWrite(record_type: RecordType) usize {
+    fn maxPayloadSizeForWrite(self: *Conn, record_type: RecordType) usize {
         if (record_type != .application_data) {
             return max_plain_text;
         }
+        _ = self;
         @panic("not implemented yet");
+    }
+
+    fn write(self: *Conn, allocator: mem.Allocator, data: []const u8) !void {
+        if (self.buffering) {
+            try self.send_buf.append(allocator, data);
+            return;
+        }
+
+        try self.stream.writer().writeAll(self.send_buf.items);
+        self.bytes_sent += self.send_buf.items.len;
+    }
+
+    fn flush(self: *Conn) !void {
+        if (self.send_buf.items.len == 0) {
+            return;
+        }
+
+        try self.stream.writer().writeAll(self.send_buf.items);
+        self.bytes_sent += self.send_buf.items.len;
+        self.send_buf.clearRetainingCapacity();
+        self.buffering = false;
     }
 };
 
