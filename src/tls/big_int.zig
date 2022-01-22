f2a7f9e43a10bd6c4e42513ffc1074464af19155
Hiroaki Nakamura <hnakamur@gmail.com>
2022-01-22 13:35:44 +0900

    WIP Add big_int gcd

--- src/tls/big_int.zig
+++ src/tls/big_int.zig
@@ -41,55 +41,216 @@ pub fn bigIntConstExp(
         if (m.eqZero()) {
             return try bigIntConstClone(allocator, big_one);
         }
+        // for y < 0: x**y mod m == (x**(-1))**|y| mod m
         @panic("not implemented yet");
     }
     const m_abs = m.abs();
-    var z = try bigIntConstExpNN(allocator, x2, y, m_abs);
-    if (!z.positive and !m.eqZero()) {
+    var z = try bigIntConstExpNN(allocator, x2.abs(), y.abs(), m_abs);
+    // std.log.debug("z.limbs.ptr=0x{x}", .{@ptrToInt(z.limbs.ptr)});
+    z.setSign(!(!z.eqZero() and !x.positive and !y.eqZero() and y.limbs[0] & 1 == 1));
+    if (!z.isPositive() and !m.eqZero()) {
         // make modulus result positive
-        var z2 = try z.toManaged(allocator);
-        try z2.sub(m_abs, z);
-        return z2.toConst();
+        // z == x**y mod |m| && 0 <= z < |m|
+        try z.sub(m_abs, z.toConst().abs());
+        // std.log.debug("z.limbs.ptr#2=0x{x}", .{@ptrToInt(z.limbs.ptr)});
+        return z.toConst().abs();
     }
-    return z;
+    return z.toConst();
+}
+
+// bigIntConstModInverse returns the multiplicative inverse of g in the ring ℤ/nℤ.
+// If g and n are not relatively prime, g has no multiplicative
+// inverse in the ring ℤ/nℤ.  In this case, returns a zero.
+pub fn bigIntConstModInverse(
+    allocator: mem.Allocator,
+    g: math.big.int.Const,
+    n: math.big.int.Const,
+) !math.big.int.Const {
+    // GCD expects parameters a and b to be > 0.
+    var n2 = n;
+    if (!n.positive) {
+        n2 = n.negate();
+    }
+
+    var g2 = try g.toManaged(allocator);
+    defer g2.deinit();
+    if (!g.positive) {
+        try mod(&g2, g, n);
+    }
+
+    @panic("not implemented yet");
+}
+
+// mod sets r to the modulus x%y for y != 0.
+// If y == 0, a division-by-zero run-time panic occurs.
+// mod implements Euclidean modulus (unlike Go).
+fn mod(
+    r: *math.big.int.Managed,
+    x: math.big.int.Const,
+    y: math.big.int.Const,
+) !void {
+    var q = try math.big.int.Managed.init(r.allocator);
+    defer q.deinit();
+    try q.divTrunc(&r, x, y);
+}
+
+// GCD sets z to the greatest common divisor of a and b and returns z.
+// If x or y are not nil, GCD sets their value such that z = a*x + b*y.
+//
+// a and b may be positive, zero or negative. (Before Go 1.14 both had
+// to be > 0.) Regardless of the signs of a and b, z is always >= 0.
+//
+// If a == b == 0, GCD sets z = x = y = 0.
+//
+// If a == 0 and b != 0, GCD sets z = |b|, x = 0, y = sign(b) * 1.
+//
+// If a != 0 and b == 0, GCD sets z = |a|, x = sign(a) * 1, y = 0.
+fn gcd(
+    d: *math.big.int.Managed,
+    x: ?*math.big.int.Managed,
+    y: ?*math.big.int.Managed,
+    a: math.big.int.Managed,
+    b: math.big.int.Managed,
+) !void {
+    try d.gcd(a, b);
+
+    if (x) |x_out| {
+        if (a.eqZero()) {
+            try x_out.set(0);
+        } else if (b.eqZero()) {
+            try x_out.set(if (a.isPositive()) @as(i8, 1) else @as(i8, -1));
+        } else {
+            // TODO: implement
+        }
+    }
+    if (y) |y_out| {
+        if (b.eqZero()) {
+            try y_out.set(0);
+        } else if (a.eqZero()) {
+            try y_out.set(if (b.isPositive()) @as(i8, 1) else @as(i8, -1));
+        } else {
+            // TODO: implement
+        }
+    }
 }
 
 // bigIntConstExpNN returns x**y mod m if m != 0,
 // otherwise it returns x**y.
 fn bigIntConstExpNN(
     allocator: mem.Allocator,
-    x: math.big.int.Const,
-    y: math.big.int.Const,
-    m: math.big.int.Const,
-) !math.big.int.Const {
+    x_abs: math.big.int.Const,
+    y_abs: math.big.int.Const,
+    m_abs: math.big.int.Const,
+) !math.big.int.Managed {
     // x**y mod 1 == 0
-    if (m.eq(big_one)) {
-        return try bigIntConstClone(allocator, big_zero);
+    if (m_abs.eq(big_one)) {
+        return try big_zero.toManaged(allocator);
     }
     // m == 0 || m > 1
 
     // x**0 == 1
-    if (y.eq(big_zero)) {
-        return try bigIntConstClone(allocator, big_one);
+    if (y_abs.eq(big_zero)) {
+        return try big_one.toManaged(allocator);
     }
     // y > 0
 
     // x**1 mod m == x mod m
-    if (y.eq(big_one) and !m.eqZero()) {
+    if (y_abs.eq(big_one) and !m_abs.eqZero()) {
         var q = try math.big.int.Managed.init(allocator);
         defer q.deinit();
         var r = try math.big.int.Managed.init(allocator);
         errdefer r.deinit();
-        try math.big.int.Managed.divFloor(&q, &r, x, m);
-        return r.toConst();
+        // std.log.debug("r.limbs.ptr=0x{x}", .{@ptrToInt(r.limbs.ptr)});
+        try q.divFloor(&r, x_abs, m_abs);
+        return r;
     }
     // y > 1
 
-    // if (!m.eqZero()) {
+    // We likely end up being as long as the modulus.
+    var z = try math.big.int.Managed.initCapacity(allocator, m_abs.limbs.len);
+    defer z.deinit();
+    try z.copy(x_abs);
 
-    // }
-    _ = x;
-    @panic("not implemented yet");
+    // If the base is non-trivial and the exponent is large, we use
+    // 4-bit, windowed exponentiation. This involves precomputing 14 values
+    // (x^2...x^15) but then reduces the number of multiply-reduces by a
+    // third. Even for a 32-bit exponent, this reduces the number of
+    // operations. Uses Montgomery method for odd moduli.
+    if (x_abs.order(big_one) == .gt and y_abs.limbs.len > 1 and !m_abs.eqZero()) {
+        if (m_abs.limbs[0] & 1 == 1) {
+            @panic("not implemented yet#2");
+        }
+        @panic("not implemented yet#3");
+    }
+
+    var v = y_abs.limbs[y_abs.limbs.len - 1]; // v > 0 because y_abs is normalized and y_abs > 0
+    const shift = nlz(v) + 1;
+    // std.log.debug("bigIntConstExpNN v={}, shift={}", .{ v, shift });
+    v = math.shl(math.big.Limb, v, shift);
+    // std.log.debug("bigIntConstExpNN shifted v={}", .{v});
+    var q = try math.big.int.Managed.init(allocator);
+    defer q.deinit();
+
+    const mask = math.shl(math.big.Limb, 1, @bitSizeOf(math.big.Limb) - 1);
+
+    // We walk through the bits of the exponent one by one. Each time we
+    // see a bit, we square, thus doubling the power. If the bit is a one,
+    // we also multiply by x, thus adding one to the power.
+    const w = @bitSizeOf(math.big.Limb) - shift;
+    // zz and r are used to avoid allocating in mul and div as
+    // otherwise the arguments would alias.
+    var zz = try math.big.int.Managed.init(allocator);
+    defer zz.deinit();
+    var r = try math.big.int.Managed.init(allocator);
+    defer r.deinit();
+    var j: usize = 0;
+    while (j < w) : (j += 1) {
+        try zz.sqr(z.toConst());
+        zz.swap(&z);
+
+        if (v & mask != 0) {
+            try zz.mul(z.toConst(), x_abs);
+            zz.swap(&z);
+        }
+
+        if (!m_abs.eqZero()) {
+            try zz.divFloor(&r, z.toConst(), m_abs);
+            zz.swap(&q);
+            z.swap(&r);
+        }
+
+        v = math.shl(math.big.Limb, v, 1);
+    }
+
+    var i: isize = @intCast(isize, y_abs.limbs.len) - 2;
+    while (i >= 0) : (i -= 1) {
+        v = y_abs.limbs[@intCast(usize, i)];
+
+        j = 0;
+        while (j < @bitSizeOf(math.big.Limb)) : (j += 1) {
+            try zz.sqr(z.toConst());
+            zz.swap(&z);
+
+            if (v & mask != 0) {
+                try zz.mul(z.toConst(), x_abs);
+                zz.swap(&z);
+            }
+
+            if (!m_abs.eqZero()) {
+                try zz.divFloor(&r, z.toConst(), m_abs);
+                zz.swap(&q);
+                z.swap(&r);
+            }
+
+            v = math.shl(math.big.Limb, v, 1);
+        }
+    }
+    return try z.clone();
+}
+
+// nlz returns the number of leading zeros in x.
+fn nlz(x: math.big.Limb) usize {
+    return @clz(math.big.Limb, x);
 }
 
 fn bigIntConstClone(
@@ -155,6 +316,7 @@ test "bigIntConstExp" {
 
             var got = try bigIntConstExp(allocator, x_i, y_i, m_i);
             defer allocator.free(got.limbs);
+            // std.log.debug("got.limbs.ptr=0x{x}", .{@ptrToInt(got.limbs.ptr)});
             if (!got.eq(out_i)) {
                 var got_s = try got.toStringAlloc(allocator, 10, .lower);
                 defer allocator.free(got_s);
@@ -193,5 +355,141 @@ test "bigIntConstExp" {
     try f(10, "-5", 10, "-7", 10, "0", 10, "1");
     try f(10, "5", 10, "0", 10, "0", 10, "1");
     try f(10, "-5", 10, "0", 10, "0", 10, "1");
-    // try f(10, "5", 10, "1", 10, "0", 10, "5");
+    try f(10, "5", 10, "1", 10, "0", 10, "5");
+    try f(10, "-5", 10, "1", 10, "0", 10, "-5");
+    try f(10, "-5", 10, "1", 10, "7", 10, "2");
+    try f(10, "-2", 10, "3", 10, "2", 10, "0");
+    try f(10, "5", 10, "2", 10, "0", 10, "25");
+    try f(10, "1", 10, "65537", 10, "2", 10, "1");
+    try f(16, "8000000000000000", 10, "2", 10, "0", 16, "40000000000000000000000000000000");
+    try f(16, "8000000000000000", 10, "2", 10, "6719", 10, "4944");
+    try f(16, "8000000000000000", 10, "3", 10, "6719", 10, "5447");
+    try f(16, "8000000000000000", 10, "1000", 10, "6719", 10, "1603");
+    try f(16, "8000000000000000", 10, "1000000", 10, "6719", 10, "3199");
+}
+
+test "bigIntDivTrunc" {
+    // Zig's std.math.big.int.Managed.divTrunc equals to Go's math/big.QuoRem.
+    const f = struct {
+        fn f(x: i64, y: i64, q: i64, r: i64, d: i64, m: i64) !void {
+            _ = d;
+            _ = m;
+            const allocator = testing.allocator;
+            { // no alias
+                var big_x = try math.big.int.Managed.initSet(allocator, x);
+                defer big_x.deinit();
+                var big_y = try math.big.int.Managed.initSet(allocator, y);
+                defer big_y.deinit();
+                var big_q = try math.big.int.Managed.init(allocator);
+                defer big_q.deinit();
+                var big_r = try math.big.int.Managed.init(allocator);
+                defer big_r.deinit();
+                try big_q.divTrunc(&big_r, big_x.toConst(), big_y.toConst());
+                try testing.expectEqual(q, try big_q.to(i64));
+                try testing.expectEqual(r, try big_r.to(i64));
+            }
+            // Though these test passes, Managed.divTrunc document says nothing
+            // about aliases, and Mutable.divTrunc document says q may alias with
+            // a or b, but says nothing about r.
+            { // alias r and x, q and y
+                var big_q = try math.big.int.Managed.initSet(allocator, y);
+                defer big_q.deinit();
+                var big_r = try math.big.int.Managed.initSet(allocator, x);
+                defer big_r.deinit();
+                try big_q.divTrunc(&big_r, big_r.toConst(), big_q.toConst());
+                try testing.expectEqual(q, try big_q.to(i64));
+                try testing.expectEqual(r, try big_r.to(i64));
+            }
+            { // alias q and x, r and y
+                var big_q = try math.big.int.Managed.initSet(allocator, x);
+                defer big_q.deinit();
+                var big_r = try math.big.int.Managed.initSet(allocator, y);
+                defer big_r.deinit();
+                try big_q.divTrunc(&big_r, big_q.toConst(), big_r.toConst());
+                try testing.expectEqual(q, try big_q.to(i64));
+                try testing.expectEqual(r, try big_r.to(i64));
+            }
+        }
+    }.f;
+
+    try f(5, 3, 1, 2, 1, 2);
+    try f(-5, 3, -1, -2, -2, 1);
+    try f(5, -3, -1, 2, -1, 2);
+    try f(-5, -3, 1, -2, 2, 1);
+    try f(1, 2, 0, 1, 0, 1);
+    try f(8, 4, 2, 0, 2, 0);
+}
+
+test "gcd" {
+    const f = struct {
+        fn f(d: []const u8, x: []const u8, y: []const u8, a: []const u8, b: []const u8) !void {
+            const allocator = testing.allocator;
+
+            var big_a = try strToManaged(allocator, a);
+            defer big_a.deinit();
+            var big_b = try strToManaged(allocator, b);
+            defer big_b.deinit();
+
+            var want_d = try strToManaged(allocator, d);
+            defer want_d.deinit();
+            var want_x = try strToManaged(allocator, x);
+            defer want_x.deinit();
+            var want_y = try strToManaged(allocator, y);
+            defer want_y.deinit();
+
+            {
+                var got_d = try math.big.int.Managed.init(allocator);
+                defer got_d.deinit();
+                try gcd(&got_d, null, null, big_a, big_b);
+                if (!got_d.eq(want_d)) {
+                    std.debug.print("gcd d mismatch, got={}, want={}\n", .{ got_d, want_d });
+                    return error.TestExpectedError;
+                }
+            }
+            {
+                var got_d = try math.big.int.Managed.init(allocator);
+                defer got_d.deinit();
+                var got_x = try math.big.int.Managed.init(allocator);
+                defer got_x.deinit();
+                try gcd(&got_d, &got_x, null, big_a, big_b);
+                if (!got_d.eq(want_d)) {
+                    std.debug.print("gcd d mismatch, got={}, want={}\n", .{ got_d, want_d });
+                    return error.TestExpectedError;
+                }
+                if (!got_x.eq(want_x)) {
+                    std.debug.print("gcd x mismatch, got={}, want={}\n", .{ got_x, want_x });
+                    return error.TestExpectedError;
+                }
+            }
+        }
+
+        fn strToManaged(allocator: mem.Allocator, value: []const u8) !math.big.int.Managed {
+            var m = try math.big.int.Managed.init(allocator);
+            errdefer m.deinit();
+            try m.setString(10, value);
+            return m;
+        }
+    }.f;
+
+    // a <= 0 || b <= 0
+    try f("0", "0", "0", "0", "0");
+    try f("7", "0", "1", "0", "7");
+    try f("7", "0", "-1", "0", "-7");
+    try f("11", "1", "0", "11", "0");
+    try f("7", "-1", "-2", "-77", "35");
+    try f("935", "-3", "8", "64515", "24310");
+    try f("935", "-3", "-8", "64515", "-24310");
+    try f("935", "3", "-8", "-64515", "-24310");
+
+    try f("1", "-9", "47", "120", "23");
+    try f("7", "1", "-2", "77", "35");
+    try f("935", "-3", "8", "64515", "24310");
+    try f("935000000000000000", "-3", "8", "64515000000000000000", "24310000000000000000");
+    try f(
+        "1",
+        "-221",
+        "22059940471369027483332068679400581064239780177629666810348940098015901108344",
+        "98920366548084643601728869055592650835572950932266967461790948584315647051443",
+        "991",
+    );
 }
--- src/tls/big_int_test.go
+++ src/tls/big_int_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"math/big"
+	"math/bits"
 	"testing"
 )
 
@@ -20,4 +21,66 @@ func TestBigIntSetStringEmpty(t *testing.T) {
 		t.Fatal("failed to set string")
 	}
 	log.Printf("i=%s", i.String())
+}
+
+func TestBitsLeadingZeros(t *testing.T) {
+	if got, want := bits.LeadingZeros(1), 63; got != want {
+		t.Errorf("result mismatch, got=%d, want=%d", got, want)
+	}
+}
+
+func TestBigIntDivMod(t *testing.T) {
+	testCases := []struct {
+		x, y int64
+		q, r int64 // T-division
+		d, m int64 // Euclidean division
+	}{
+		{5, 3, 1, 2, 1, 2},
+		{-5, 3, -1, -2, -2, 1},
+		{5, -3, -1, 2, -1, 2},
+		{-5, -3, 1, -2, 2, 1},
+		{1, 2, 0, 1, 0, 1},
+		{8, 4, 2, 0, 2, 0},
+	}
+	for _, c := range testCases {
+		x := big.NewInt(c.x)
+		y := big.NewInt(c.y)
+
+		q1 := new(big.Int).Quo(x, y)
+		if got, want := q1.Int64(), c.q; got != want {
+			t.Errorf("Quo mismatch, got=%d, want=%d", got, want)
+		}
+
+		r1 := new(big.Int).Rem(x, y)
+		if got, want := r1.Int64(), c.r; got != want {
+			t.Errorf("Rem mismatch, got=%d, want=%d", got, want)
+		}
+
+		q2, r2 := new(big.Int).QuoRem(x, y, new(big.Int))
+		if got, want := q2.Int64(), c.q; got != want {
+			t.Errorf("QuoRem quo mismatch, got=%d, want=%d", got, want)
+		}
+		if got, want := r2.Int64(), c.r; got != want {
+			t.Errorf("QuoRem rem mismatch, got=%d, want=%d", got, want)
+		}
+
+		d1 := new(big.Int).Div(x, y)
+		if got, want := d1.Int64(), c.d; got != want {
+			t.Errorf("Div mismatch, got=%d, want=%d", got, want)
+		}
+
+		m1 := new(big.Int).Mod(x, y)
+		if got, want := m1.Int64(), c.m; got != want {
+			t.Errorf("Mod mismatch, got=%d, want=%d", got, want)
+		}
+
+		d2, m2 := new(big.Int).DivMod(x, y, new(big.Int))
+		if got, want := d2.Int64(), c.d; got != want {
+			t.Errorf("DivMod div mismatch, got=%d, want=%d", got, want)
+		}
+		if got, want := m2.Int64(), c.m; got != want {
+			t.Errorf("DivMod mod mismatch, got=%d, want=%d", got, want)
+		}
+	}
+
 }
