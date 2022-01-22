6c76a00de40c4e53a25b5dab791b6cb3cda29f5d
Hiroaki Nakamura <hnakamur@gmail.com>
2022-01-23 05:52:26 +0900

    Add expNnWindowed with bug

--- src/tls/big_int.zig
+++ src/tls/big_int.zig
@@ -712,7 +712,7 @@ fn expNn(
         if (m_abs.limbs[0] & 1 == 1) {
             return try expNnMontgomery(allocator, x_abs, y_abs, m_abs);
         }
-        @panic("not implemented yet#3");
+        return try expNnWindowed(allocator, x_abs, y_abs, m_abs);
     }
 
     var v = y_abs.limbs[y_abs_limbs_len - 1]; // v > 0 because y_abs is normalized and y_abs > 0
@@ -778,6 +778,96 @@ fn expNn(
         }
     }
     return try z.clone();
+}
+
+/// expNnWindowed calculates x**y mod m using a fixed, 4-bit window.
+fn expNnWindowed(
+    allocator: mem.Allocator,
+    x_abs: Const,
+    y_abs: Const,
+    m_abs: Const,
+) !Managed {
+    std.log.debug("expNnWindowed start", .{});
+
+    // zz and r are used to avoid allocating in mul and div as otherwise
+    // the arguments would alias.
+    var zz = try Managed.init(allocator);
+    defer zz.deinit();
+    var r = try Managed.init(allocator);
+    defer r.deinit();
+
+    const n = 4;
+    // powers[i] contains x^i.
+    var powers = try allocator.alloc(Managed, 1 << n);
+    defer {
+        for (powers) |*p| p.deinit();
+        allocator.free(powers);
+    }
+
+    powers[0] = try Managed.initSet(allocator, 1);
+    powers[1] = try Managed.init(allocator);
+    try powers[1].copy(x_abs);
+    var i: usize = 2;
+    while (i < powers.len) : (i += 1) {
+        powers[i] = try Managed.initSet(allocator, 0);
+    }
+    i = 2;
+    while (i < 1 << n) : (i += 2) {
+        var p2 = &powers[i / 2];
+        var p = &powers[i];
+        var p1 = &powers[i + 1];
+        try p.sqr(p2.toConst());
+        try zz.divTrunc(&r, p.toConst(), m_abs);
+        p.swap(&r);
+        try p1.mul(p.toConst(), x_abs);
+        try zz.divTrunc(&r, p1.toConst(), m_abs);
+        p1.swap(&r);
+    }
+
+    var z = try Managed.initSet(allocator, 1);
+    i = y_abs.limbs.len - 1;
+    while (i >= 0) : (i -= 1) {
+        var yi = y_abs.limbs[i];
+        var j: usize = 0;
+        while (j < @bitSizeOf(Limb)) : (j += n) {
+            if (i != y_abs.limbs.len - 1 or j != 0) {
+                // Unrolled loop for significant performance
+                // gain. Use go test -bench=".*" in crypto/rsa
+                // to check performance before making changes.
+                try zz.sqr(z.toConst());
+                zz.swap(&z);
+                try zz.divTrunc(&r, z.toConst(), m_abs);
+                z.swap(&r);
+
+                try zz.sqr(z.toConst());
+                zz.swap(&z);
+                try zz.divTrunc(&r, z.toConst(), m_abs);
+                z.swap(&r);
+
+                try zz.sqr(z.toConst());
+                zz.swap(&z);
+                try zz.divTrunc(&r, z.toConst(), m_abs);
+                z.swap(&r);
+
+                try zz.sqr(z.toConst());
+                zz.swap(&z);
+                try zz.divTrunc(&r, z.toConst(), m_abs);
+                z.swap(&r);
+            }
+
+            try zz.mul(z.toConst(), powers[yi >> (@bitSizeOf(Limb) - n)].toConst());
+            zz.swap(&z);
+            try zz.divTrunc(&r, z.toConst(), m_abs);
+            z.swap(&r);
+
+            yi <<= n;
+        }
+        if (i == 0) {
+            break;
+        }
+    }
+    z.normalize(z.limbs.len);
+    return z;
 }
 
 fn expNnMontgomery(
@@ -1176,6 +1266,17 @@ test "expConst" {
         "01112222333344445555666677778889",
         16,
         "36168FA1DB3AAE6C8CE647E137F97A",
+    );
+
+    try f(
+        10,
+        "2938462938472983472983659726349017249287491026512746239764525612965293865296239471239874193284792387498274256129746192347",
+        10,
+        "298472983472983471903246121093472394872319615612417471234712061",
+        10,
+        "29834729834729834729347290846729561262544958723956495615629569234729836259263598127342374289365912465901365498236492183464",
+        10,
+        "23537740700184054162508175125554701713153216681790245129157191391322321508055833908509185839069455749219131480588829346291",
     );
 }
 
