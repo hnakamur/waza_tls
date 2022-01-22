a338e8551b497252e0bf688a3fdb71f1942e2133
Hiroaki Nakamura <hnakamur@gmail.com>
2022-01-22 15:52:23 +0900

    Copy stdlib's gcd

--- src/tls/big_int.zig
+++ src/tls/big_int.zig
@@ -1,14 +1,22 @@
 const std = @import("std");
+const assert = std.debug.assert;
 const math = std.math;
 const mem = std.mem;
 
-const big_zero = math.big.int.Const{ .limbs = &[_]math.big.Limb{0}, .positive = true };
-const big_one = math.big.int.Const{ .limbs = &[_]math.big.Limb{1}, .positive = true };
+const Limb = std.math.big.Limb;
+const limb_bits = @typeInfo(Limb).Int.bits;
+const DoubleLimb = std.math.big.DoubleLimb;
+const SignedDoubleLimb = std.math.big.SignedDoubleLimb;
+const Const = std.math.big.int.Const;
+const Mutable = std.math.big.int.Mutable;
+const Managed = std.math.big.int.Managed;
+
+const big_zero = Const{ .limbs = &[_]Limb{0}, .positive = true };
+const big_one = Const{ .limbs = &[_]Limb{1}, .positive = true };
 
 // setBytes interprets buf as the bytes of a big-endian unsigned
 // integer, sets z to that value, and returns z.
-pub fn bigIntConstFromBytes(allocator: mem.Allocator, buf: []const u8) !math.big.int.Const {
-    const Limb = math.big.Limb;
+pub fn bigIntConstFromBytes(allocator: mem.Allocator, buf: []const u8) !Const {
     var limbs = try allocator.alloc(Limb, try math.divCeil(usize, buf.len, @sizeOf(Limb)));
     errdefer allocator.free(limbs);
 
@@ -20,7 +28,7 @@ pub fn bigIntConstFromBytes(allocator: mem.Allocator, buf: []const u8) !math.big
     }
     mem.set(u8, limbs_bytes[i .. limbs.len * @sizeOf(Limb)], 0);
 
-    return math.big.int.Const{ .limbs = limbs, .positive = true };
+    return Const{ .limbs = limbs, .positive = true };
 }
 
 // bigIntConstExp returns x**y mod |m| (i.e. the sign of m is ignored).
@@ -31,10 +39,10 @@ pub fn bigIntConstFromBytes(allocator: mem.Allocator, buf: []const u8) !math.big
 // cryptographically constant-time operation.
 pub fn bigIntConstExp(
     allocator: mem.Allocator,
-    x: math.big.int.Const,
-    y: math.big.int.Const,
-    m: math.big.int.Const,
-) !math.big.int.Const {
+    x: Const,
+    y: Const,
+    m: Const,
+) !Const {
     // See Knuth, volume 2, section 4.6.3.
     var x2 = x;
     if (!y.positive) {
@@ -63,9 +71,9 @@ pub fn bigIntConstExp(
 // inverse in the ring ℤ/nℤ.  In this case, returns a zero.
 pub fn bigIntConstModInverse(
     allocator: mem.Allocator,
-    g: math.big.int.Const,
-    n: math.big.int.Const,
-) !math.big.int.Const {
+    g: Const,
+    n: Const,
+) !Const {
     // GCD expects parameters a and b to be > 0.
     var n2 = n;
     if (!n.positive) {
@@ -85,11 +93,11 @@ pub fn bigIntConstModInverse(
 // If y == 0, a division-by-zero run-time panic occurs.
 // mod implements Euclidean modulus (unlike Go).
 fn mod(
-    r: *math.big.int.Managed,
-    x: math.big.int.Const,
-    y: math.big.int.Const,
+    r: *Managed,
+    x: Const,
+    y: Const,
 ) !void {
-    var q = try math.big.int.Managed.init(r.allocator);
+    var q = try Managed.init(r.allocator);
     defer q.deinit();
     try q.divTrunc(&r, x, y);
 }
@@ -106,13 +114,19 @@ fn mod(
 //
 // If a != 0 and b == 0, GCD sets z = |a|, x = sign(a) * 1, y = 0.
 fn gcd(
-    d: *math.big.int.Managed,
-    x: ?*math.big.int.Managed,
-    y: ?*math.big.int.Managed,
-    a: math.big.int.Managed,
-    b: math.big.int.Managed,
+    d: *Managed,
+    x: ?*Managed,
+    y: ?*Managed,
+    a: Managed,
+    b: Managed,
 ) !void {
-    try d.gcd(a, b);
+    // try d.gcd(a, b);
+    try gcdManaged(d, a, b);
+
+    // a = aa * d;
+    // b = bb * d;
+    // d = a * x + b + y
+    //   = aa * d * x + bb * d * y;
 
     if (x) |x_out| {
         if (a.eqZero()) {
@@ -134,14 +148,175 @@ fn gcd(
     }
 }
 
+/// rma may alias x or y.
+/// x and y may alias each other.
+///
+/// rma's allocator is used for temporary storage to boost multiplication performance.
+pub fn gcdManaged(
+    rma: *Managed,
+    x: Managed,
+    y: Managed,
+) !void {
+    try rma.ensureCapacity(math.min(x.len(), y.len()));
+    var m = rma.toMutable();
+    var limbs_buffer = std.ArrayList(Limb).init(rma.allocator);
+    defer limbs_buffer.deinit();
+    try gcdMutable(&m, x.toConst(), y.toConst(), &limbs_buffer);
+    rma.setMetadata(m.positive, m.len);
+}
+
+/// rma may alias x or y.
+/// x and y may alias each other.
+/// Asserts that `rma` has enough limbs to store the result. Upper bound is
+/// `math.min(x.limbs.len, y.limbs.len)`.
+///
+/// `limbs_buffer` is used for temporary storage during the operation. When this function returns,
+/// it will have the same length as it had when the function was called.
+pub fn gcdMutable(
+    rma: *Mutable,
+    x: Const,
+    y: Const,
+    limbs_buffer: *std.ArrayList(Limb),
+) !void {
+    const prev_len = limbs_buffer.items.len;
+    defer limbs_buffer.shrinkRetainingCapacity(prev_len);
+    const x_copy = if (rma.limbs.ptr == x.limbs.ptr) blk: {
+        const start = limbs_buffer.items.len;
+        try limbs_buffer.appendSlice(x.limbs);
+        break :blk x.toMutable(limbs_buffer.items[start..]).toConst();
+    } else x;
+    const y_copy = if (rma.limbs.ptr == y.limbs.ptr) blk: {
+        const start = limbs_buffer.items.len;
+        try limbs_buffer.appendSlice(y.limbs);
+        break :blk y.toMutable(limbs_buffer.items[start..]).toConst();
+    } else y;
+
+    return gcdLehmer(rma, x_copy, y_copy, limbs_buffer);
+}
+
+fn gcdLehmer(
+    result: *Mutable,
+    xa: Const,
+    ya: Const,
+    limbs_buffer: *std.ArrayList(Limb),
+) !void {
+    var x = try xa.toManaged(limbs_buffer.allocator);
+    defer x.deinit();
+    x.abs();
+
+    var y = try ya.toManaged(limbs_buffer.allocator);
+    defer y.deinit();
+    y.abs();
+
+    if (x.toConst().order(y.toConst()) == .lt) {
+        x.swap(&y);
+    }
+
+    var t_big = try Managed.init(limbs_buffer.allocator);
+    defer t_big.deinit();
+
+    var r = try Managed.init(limbs_buffer.allocator);
+    defer r.deinit();
+
+    var tmp_x = try Managed.init(limbs_buffer.allocator);
+    defer tmp_x.deinit();
+
+    while (y.len() > 1) {
+        assert(x.isPositive() and y.isPositive());
+        assert(x.len() >= y.len());
+
+        var xh: SignedDoubleLimb = x.limbs[x.len() - 1];
+        var yh: SignedDoubleLimb = if (x.len() > y.len()) 0 else y.limbs[x.len() - 1];
+
+        var A: SignedDoubleLimb = 1;
+        var B: SignedDoubleLimb = 0;
+        var C: SignedDoubleLimb = 0;
+        var D: SignedDoubleLimb = 1;
+
+        while (yh + C != 0 and yh + D != 0) {
+            const q = @divFloor(xh + A, yh + C);
+            const qp = @divFloor(xh + B, yh + D);
+            if (q != qp) {
+                break;
+            }
+
+            var t = A - q * C;
+            A = C;
+            C = t;
+            t = B - q * D;
+            B = D;
+            D = t;
+
+            t = xh - q * yh;
+            xh = yh;
+            yh = t;
+        }
+
+        if (B == 0) {
+            // t_big = x % y, r is unused
+            try r.divTrunc(&t_big, x.toConst(), y.toConst());
+            assert(t_big.isPositive());
+
+            x.swap(&y);
+            y.swap(&t_big);
+        } else {
+            var storage: [8]Limb = undefined;
+            const Ap = fixedIntFromSignedDoubleLimb(A, storage[0..2]).toConst();
+            const Bp = fixedIntFromSignedDoubleLimb(B, storage[2..4]).toConst();
+            const Cp = fixedIntFromSignedDoubleLimb(C, storage[4..6]).toConst();
+            const Dp = fixedIntFromSignedDoubleLimb(D, storage[6..8]).toConst();
+
+            // t_big = Ax + By
+            try r.mul(x.toConst(), Ap);
+            try t_big.mul(y.toConst(), Bp);
+            try t_big.add(r.toConst(), t_big.toConst());
+
+            // u = Cx + Dy, r as u
+            try tmp_x.copy(x.toConst());
+            try x.mul(tmp_x.toConst(), Cp);
+            try r.mul(y.toConst(), Dp);
+            try r.add(x.toConst(), r.toConst());
+
+            x.swap(&t_big);
+            y.swap(&r);
+        }
+    }
+
+    // euclidean algorithm
+    assert(x.toConst().order(y.toConst()) != .lt);
+
+    while (!y.toConst().eqZero()) {
+        try t_big.divTrunc(&r, x.toConst(), y.toConst());
+        x.swap(&y);
+        y.swap(&r);
+    }
+
+    result.copy(x.toConst());
+}
+
+// Storage must live for the lifetime of the returned value
+fn fixedIntFromSignedDoubleLimb(A: SignedDoubleLimb, storage: []Limb) Mutable {
+    assert(storage.len >= 2);
+
+    const A_is_positive = A >= 0;
+    const Au = @intCast(DoubleLimb, if (A < 0) -A else A);
+    storage[0] = @truncate(Limb, Au);
+    storage[1] = @truncate(Limb, Au >> limb_bits);
+    return .{
+        .limbs = storage[0..2],
+        .positive = A_is_positive,
+        .len = 2,
+    };
+}
+
 // bigIntConstExpNN returns x**y mod m if m != 0,
 // otherwise it returns x**y.
 fn bigIntConstExpNN(
     allocator: mem.Allocator,
-    x_abs: math.big.int.Const,
-    y_abs: math.big.int.Const,
-    m_abs: math.big.int.Const,
-) !math.big.int.Managed {
+    x_abs: Const,
+    y_abs: Const,
+    m_abs: Const,
+) !Managed {
     // x**y mod 1 == 0
     if (m_abs.eq(big_one)) {
         return try big_zero.toManaged(allocator);
@@ -156,9 +331,9 @@ fn bigIntConstExpNN(
 
     // x**1 mod m == x mod m
     if (y_abs.eq(big_one) and !m_abs.eqZero()) {
-        var q = try math.big.int.Managed.init(allocator);
+        var q = try Managed.init(allocator);
         defer q.deinit();
-        var r = try math.big.int.Managed.init(allocator);
+        var r = try Managed.init(allocator);
         errdefer r.deinit();
         // std.log.debug("r.limbs.ptr=0x{x}", .{@ptrToInt(r.limbs.ptr)});
         try q.divFloor(&r, x_abs, m_abs);
@@ -167,7 +342,7 @@ fn bigIntConstExpNN(
     // y > 1
 
     // We likely end up being as long as the modulus.
-    var z = try math.big.int.Managed.initCapacity(allocator, m_abs.limbs.len);
+    var z = try Managed.initCapacity(allocator, m_abs.limbs.len);
     defer z.deinit();
     try z.copy(x_abs);
 
@@ -186,22 +361,22 @@ fn bigIntConstExpNN(
     var v = y_abs.limbs[y_abs.limbs.len - 1]; // v > 0 because y_abs is normalized and y_abs > 0
     const shift = nlz(v) + 1;
     // std.log.debug("bigIntConstExpNN v={}, shift={}", .{ v, shift });
-    v = math.shl(math.big.Limb, v, shift);
+    v = math.shl(Limb, v, shift);
     // std.log.debug("bigIntConstExpNN shifted v={}", .{v});
-    var q = try math.big.int.Managed.init(allocator);
+    var q = try Managed.init(allocator);
     defer q.deinit();
 
-    const mask = math.shl(math.big.Limb, 1, @bitSizeOf(math.big.Limb) - 1);
+    const mask = math.shl(Limb, 1, @bitSizeOf(Limb) - 1);
 
     // We walk through the bits of the exponent one by one. Each time we
     // see a bit, we square, thus doubling the power. If the bit is a one,
     // we also multiply by x, thus adding one to the power.
-    const w = @bitSizeOf(math.big.Limb) - shift;
+    const w = @bitSizeOf(Limb) - shift;
     // zz and r are used to avoid allocating in mul and div as
     // otherwise the arguments would alias.
-    var zz = try math.big.int.Managed.init(allocator);
+    var zz = try Managed.init(allocator);
     defer zz.deinit();
-    var r = try math.big.int.Managed.init(allocator);
+    var r = try Managed.init(allocator);
     defer r.deinit();
     var j: usize = 0;
     while (j < w) : (j += 1) {
@@ -219,7 +394,7 @@ fn bigIntConstExpNN(
             z.swap(&r);
         }
 
-        v = math.shl(math.big.Limb, v, 1);
+        v = math.shl(Limb, v, 1);
     }
 
     var i: isize = @intCast(isize, y_abs.limbs.len) - 2;
@@ -227,7 +402,7 @@ fn bigIntConstExpNN(
         v = y_abs.limbs[@intCast(usize, i)];
 
         j = 0;
-        while (j < @bitSizeOf(math.big.Limb)) : (j += 1) {
+        while (j < @bitSizeOf(Limb)) : (j += 1) {
             try zz.sqr(z.toConst());
             zz.swap(&z);
 
@@ -242,39 +417,39 @@ fn bigIntConstExpNN(
                 z.swap(&r);
             }
 
-            v = math.shl(math.big.Limb, v, 1);
+            v = math.shl(Limb, v, 1);
         }
     }
     return try z.clone();
 }
 
 // nlz returns the number of leading zeros in x.
-fn nlz(x: math.big.Limb) usize {
-    return @clz(math.big.Limb, x);
+fn nlz(x: Limb) usize {
+    return @clz(Limb, x);
 }
 
 fn bigIntConstClone(
     allocator: mem.Allocator,
-    x: math.big.int.Const,
-) !math.big.int.Const {
-    return math.big.int.Const{
-        .limbs = try allocator.dupe(math.big.Limb, x.limbs),
+    x: Const,
+) !Const {
+    return Const{
+        .limbs = try allocator.dupe(Limb, x.limbs),
         .positive = x.positive,
     };
 }
 
 const testing = std.testing;
 
-test "std.math.big.int.Const const" {
+test "std.Const const" {
     try testing.expectEqual(@as(u64, 0), try big_zero.to(u64));
     try testing.expectEqual(@as(u64, 1), try big_one.to(u64));
 }
 
-test "std.math.big.int.Const zero" {
+test "std.Const zero" {
     const allocator = testing.allocator;
-    var zero = (try std.math.big.int.Managed.initSet(allocator, 0)).toConst();
+    var zero = (try std.Managed.initSet(allocator, 0)).toConst();
     defer allocator.free(zero.limbs);
-    try testing.expectEqualSlices(std.math.big.Limb, &[_]std.math.big.Limb{0}, zero.limbs);
+    try testing.expectEqualSlices(std.Limb, &[_]std.Limb{0}, zero.limbs);
     try testing.expect(zero.positive);
 }
 
@@ -327,8 +502,8 @@ test "bigIntConstExp" {
             }
         }
 
-        fn initConst(allocator: mem.Allocator, base: u8, value: []const u8) !math.big.int.Const {
-            var m = try math.big.int.Managed.init(allocator);
+        fn initConst(allocator: mem.Allocator, base: u8, value: []const u8) !Const {
+            var m = try Managed.init(allocator);
             errdefer m.deinit();
             try m.setString(base, value);
             return m.toConst();
@@ -369,20 +544,20 @@ test "bigIntConstExp" {
 }
 
 test "bigIntDivTrunc" {
-    // Zig's std.math.big.int.Managed.divTrunc equals to Go's math/big.QuoRem.
+    // Zig's std.Managed.divTrunc equals to Go's math/big.QuoRem.
     const f = struct {
         fn f(x: i64, y: i64, q: i64, r: i64, d: i64, m: i64) !void {
             _ = d;
             _ = m;
             const allocator = testing.allocator;
             { // no alias
-                var big_x = try math.big.int.Managed.initSet(allocator, x);
+                var big_x = try Managed.initSet(allocator, x);
                 defer big_x.deinit();
-                var big_y = try math.big.int.Managed.initSet(allocator, y);
+                var big_y = try Managed.initSet(allocator, y);
                 defer big_y.deinit();
-                var big_q = try math.big.int.Managed.init(allocator);
+                var big_q = try Managed.init(allocator);
                 defer big_q.deinit();
-                var big_r = try math.big.int.Managed.init(allocator);
+                var big_r = try Managed.init(allocator);
                 defer big_r.deinit();
                 try big_q.divTrunc(&big_r, big_x.toConst(), big_y.toConst());
                 try testing.expectEqual(q, try big_q.to(i64));
@@ -392,18 +567,18 @@ test "bigIntDivTrunc" {
             // about aliases, and Mutable.divTrunc document says q may alias with
             // a or b, but says nothing about r.
             { // alias r and x, q and y
-                var big_q = try math.big.int.Managed.initSet(allocator, y);
+                var big_q = try Managed.initSet(allocator, y);
                 defer big_q.deinit();
-                var big_r = try math.big.int.Managed.initSet(allocator, x);
+                var big_r = try Managed.initSet(allocator, x);
                 defer big_r.deinit();
                 try big_q.divTrunc(&big_r, big_r.toConst(), big_q.toConst());
                 try testing.expectEqual(q, try big_q.to(i64));
                 try testing.expectEqual(r, try big_r.to(i64));
             }
             { // alias q and x, r and y
-                var big_q = try math.big.int.Managed.initSet(allocator, x);
+                var big_q = try Managed.initSet(allocator, x);
                 defer big_q.deinit();
-                var big_r = try math.big.int.Managed.initSet(allocator, y);
+                var big_r = try Managed.initSet(allocator, y);
                 defer big_r.deinit();
                 try big_q.divTrunc(&big_r, big_q.toConst(), big_r.toConst());
                 try testing.expectEqual(q, try big_q.to(i64));
@@ -438,7 +613,7 @@ test "gcd" {
             defer want_y.deinit();
 
             {
-                var got_d = try math.big.int.Managed.init(allocator);
+                var got_d = try Managed.init(allocator);
                 defer got_d.deinit();
                 try gcd(&got_d, null, null, big_a, big_b);
                 if (!got_d.eq(want_d)) {
@@ -447,9 +622,9 @@ test "gcd" {
                 }
             }
             {
-                var got_d = try math.big.int.Managed.init(allocator);
+                var got_d = try Managed.init(allocator);
                 defer got_d.deinit();
-                var got_x = try math.big.int.Managed.init(allocator);
+                var got_x = try Managed.init(allocator);
                 defer got_x.deinit();
                 try gcd(&got_d, &got_x, null, big_a, big_b);
                 if (!got_d.eq(want_d)) {
@@ -463,8 +638,8 @@ test "gcd" {
             }
         }
 
-        fn strToManaged(allocator: mem.Allocator, value: []const u8) !math.big.int.Managed {
-            var m = try math.big.int.Managed.init(allocator);
+        fn strToManaged(allocator: mem.Allocator, value: []const u8) !Managed {
+            var m = try Managed.init(allocator);
             errdefer m.deinit();
             try m.setString(10, value);
             return m;
