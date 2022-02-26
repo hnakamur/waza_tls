bc97baa50074697a2b6f0a05c44c3cd775289730
Hiroaki Nakamura <hnakamur@gmail.com>
2022-02-26 11:14:04 +0900

    Clean up go https server and client

--- src/tls/https.go
+++ src/tls/https.go
@@ -8,7 +8,9 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -36,24 +38,40 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("This is an example server.\n"))
 }
 
+const port = 8443
+
+// const host = "naruh.dev"
+// const certFilename = "naruh.dev.crt"
+// const keyFilename = "naruh.dev.key"
+// const skipVerifyCert = false
+
+const host = "my-server.example.test"
+const certFilename = "../../tests/rsa2048.crt.pem"
+const keyFilename = "../../tests/rsa2048.key.pem"
+const skipVerifyCert = true
+
 func runServer() error {
 	http.HandleFunc("/", HelloServer)
-	return http.ListenAndServeTLS(":8443", "naruh.dev.crt", "naruh.dev.key", nil)
+	return http.ListenAndServeTLS(":"+strconv.Itoa(port), certFilename, keyFilename, nil)
 }
 
 func runClient() error {
 	tr := http.DefaultTransport.(*http.Transport).Clone()
-	tr.TLSClientConfig = &tls.Config{MaxVersion: tls.VersionTLS13}
+	tr.TLSClientConfig = &tls.Config{
+		MaxVersion:         tls.VersionTLS13,
+		InsecureSkipVerify: skipVerifyCert,
+	}
 	tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
 		var d net.Dialer
-		return d.DialContext(ctx, network, "127.0.0.1:8443")
+		return d.DialContext(ctx, network, net.JoinHostPort("127.0.0.1", strconv.Itoa(port)))
 	}
 	c := http.Client{Transport: tr}
-	req, err := http.NewRequest(http.MethodGet, "https://naruh.dev:8443", nil)
+	u := url.URL{Scheme: "https", Host: net.JoinHostPort(host, strconv.Itoa(port))}
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
 		return err
 	}
-	req.Host = "naruh.dev"
+	req.Host = host
 	resp, err := c.Do(req)
 	if err != nil {
 		return err
